9ce7580851f3f7500dba134095dc9c57103e21f1
Ala Ben Mohamed <alabenmed583@gmail.com>
2024-10-15 10:28:10 +0100

    Router

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
   }, []);
 
   return (
-    <BrowserRouter basename="/portfolio">
+    <BrowserRouter>
       <Preloader load={load} />
       <div className="App" id={load ? "no-scroll" : "scroll"}>
         <Navbar />
